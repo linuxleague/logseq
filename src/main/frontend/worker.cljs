a66567280b893a8f45ff083f10ba4bc0e8060947
Tienson Qin <tiensonqin@gmail.com>
2023-09-22 19:01:20 +0800

    Add sqlite.wasm

--- src/main/frontend/worker.cljs
+++ src/main/frontend/worker.cljs
@@ -1,6 +1,18 @@
 (ns frontend.worker
-  (:require ["@sqlite.org/sqlite-wasm" :as wasm]))
+  (:require ["@sqlite.org/sqlite-wasm" :as wasm]
+            [promesa.core :as p]))
 
 (defn init
   []
   (prn :debug "Worker init"))
+
+(comment
+  (def sqlite3InitModule (.-default wasm))
+  (->
+   (p/let [base-url (str js/window.location.protocol "//" js/window.location.host)
+           sqlite (sqlite3InitModule (clj->js {:url (str base-url "/js/sqlite-wasm/")
+                                               :print js/console.log
+                                               :printErr js/console.error}))]
+     (js/console.dir (.-version sqlite)))
+   (p/catch (fn [error]
+              (js/console.error error)))))
